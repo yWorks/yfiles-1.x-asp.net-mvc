b8df75986e14c00f13cb42f3baa8bb5161019ac2
Orbifold <swa@orbifold.net>
2015-07-01 06:35:56 +0200

    Missing files.

---
+++ yFilesMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="yFilesMVC.Global" Language="C#" %>
---
+++ yFilesMVC/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("yFilesMVC")]
+[assembly: AssemblyDescription("Sample ASP.Net MVC site serving data to yFiles for HTML")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("yWorks GmbH")]
+[assembly: AssemblyProduct("yFilesMVC")]
+[assembly: AssemblyCopyright("Copyright © yWorks  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("72f1db33-d46e-42d2-b38f-132a3c05ff22")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
